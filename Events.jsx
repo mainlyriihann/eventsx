b982375d0578182ac2f2484f11c0a9d775839529
Nishant Rihan <159631600+mainlyriihann@users.noreply.github.com>
2024-08-22 12:52:33 +0530

    Add files via upload

---
+++ Events.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+
+      <table>
+        <tr>
+       <img></img>
+       <img></img>
+       <img></img>
+        <img></img>
+        <img></img>
+        </tr>
+        <tr>
+          <td>Photography</td>
+          <td>Cinematography</td>
+          <td>Animation</td>
+          <td>Design</td>
+          <td>Outreach</td>
+          <td>media</td>
+           </tr>
+      </table>
+    </div>
+  )
+}
+
+export default Events
